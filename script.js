82379beb27a5dda612a7862a83dccf75ab33781b
mbene dieye <mbenedieye757@gmail.com>
2025-05-19 20:09:26 +0200

    saltare elementi durante il destructing

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 const array =[23,25,1,13,15]
 let [primoelemento,secondoelemento]=array
 console.log("il primo è:"+ primoelemento , "il secondo elemento è:" +secondoelemento)
+
+// saltare elementi durante il destructing
+let [elemento1, ,terzaelemento]=array
+console.log("il primo è:"+ elemento1 , "il terza elemento è:" +terzaelemento)
