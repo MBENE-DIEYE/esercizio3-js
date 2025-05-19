a71d0f796ccb4c2b39741eb5f2cf74d40361ef3a
mbene dieye <mbenedieye757@gmail.com>
2025-05-19 20:25:43 +0200

    destructuring con valori predefiniti

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const array =[23,25,1,13,15]
 let [primoelemento,secondoelemento]=array
 console.log("il primo è:"+ primoelemento , "il secondo elemento è:" +secondoelemento)
 
-// saltare elementi durante il destructing
+// saltare elementi durante il destructuring
 let [elemento1, ,terzaelemento]=array
 console.log("il primo è:"+ elemento1 , "il terza elemento è:" +terzaelemento)
+
+// destructuring con valori predefiniti
+const array1=[24,45,676,67]
+let [valore1,valore2,valore3,valore4,...valoremanca] =array1
+console.log(valore1,valore2,valore3,valore4,...valoremanca)
