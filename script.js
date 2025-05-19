6e8f2cd54fa708a3785392baec2e781caa2990bf
mbene dieye <mbenedieye757@gmail.com>
2025-05-19 20:01:00 +0200

    strazioni elementi base

---
+++ script.js
@@ -0,0 +1,4 @@
+// strazioni elementi base
+const array =[23,25,1,13,15]
+let [primoelemento,secondoelemento]=array
+console.log("il primo è:"+ primoelemento , "il secondo elemento è:" +secondoelemento)
